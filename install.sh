70935a11babc7d56072b11f389c15be64c5acc3a
ricovdh <ricovanderhallen@me.com>
2022-10-15 23:01:47 +0200

    Update

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ clear
 
 mkdir /Applications/Timecode\ Offset\ Calculator
 cd /Applications/Timecode\ Offset\ Calculator
-curl -O "https://github.com/ricovdh/timecode_offset_calculator.git"
-#unzip /Applications/Timecode\ Offset\ Calculator/main.zip
+curl -LO https://github.com/ricovdh/timecode_offset_calculator/releases/download/beta/toc-0.1.zip
+unzip toc-0.1.zip
+rm toc-0.1.zip
+cp tc_calc.js TC\ Offset\ Calculator
+clear
