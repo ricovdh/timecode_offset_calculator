c203081ba46384db70951ee3b8ed3e2eff8ca2dc
ricovdh <ricovanderhallen@me.com>
2022-10-15 22:25:40 +0200

    Update

---
+++ install.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+clear
+
+mkdir /Applications/Timecode\ Offset\ Calculator
+cd /Applications/Timecode\ Offset\ Calculator
+curl -O "https://github.com/ricovdh/timecode_offset_calculator.git"
+#unzip /Applications/Timecode\ Offset\ Calculator/main.zip
